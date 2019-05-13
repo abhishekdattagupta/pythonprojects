1ded325cee59069a80b080a75777d10537a16c18
abhishekdattagupta <abhishekcoolmech@gmail.com>
2019-05-13 17:42:22 +0530

    TechKnow School NumpyAssignment 1

---
+++ NumpyPandaAssignment/NumpyAssignment1.py
@@ -0,0 +1,22 @@
+# Create an array using the method 1. array 2. arange 3. linspace
+
+import numpy as np
+
+# Using array
+
+a = np.array([1, 2, 3])
+
+print("Using array: ", a)
+print(a[1])
+
+# Using arrange
+
+b = np.arange(0, 40, 10)
+
+print("Using arrange: ", b)
+
+# Using linspace
+
+c = np.linspace(0, 1, 6)
+
+print("Using linspace: ", c)
